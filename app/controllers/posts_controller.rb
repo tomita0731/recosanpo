3324cc68fb325f1bdd87f471ccbb4b46788618f3
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2024-11-25 01:57:31 +0000

    [Add} Post_index,post_show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,19 +11,34 @@ class PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
+   @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to user_path
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to user_path(user.id)
   end
 
 
 private
 
 def post_params
-  params.require(:post).permit(:step_count, :place, :genre, :body, :genre_id)
+  params.require(:post).permit(:step_count, :place, :genre, :body, :genre_id, :image)
 end
 
 end
