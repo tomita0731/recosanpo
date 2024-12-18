835cf092750d13e220ef67202e89ae1e8c42f511
EC2 Default User <ec2-user@ip-172-31-38-133.eu-north-1.compute.internal>
2024-12-18 10:37:38 +0000

    [Add] feature/admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :destroy]
+    resources :posts, only: [:show, :destroy] do
+      resources :post_comments, only: [:destroy]
+    end
   end
-  root to:"homes#top"
-  get "homes/about"
-  get 'mypage', to: 'users#mypage'
 
-  get "search" => "searches#search"
-
-  resources :users, only: [:edit, :show, :index, :update, :destroy]
-  resources :posts, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
-    resources :post_comments, only: [:create, :destroy]
+  scope module: :public do
+    devise_for :users
+    devise_scope :user do
+      post "users_guest_sign_in", to: "sessions#guest_sign_in"
+    end
+    root to: "homes#top"
+    get "homes/about"
+    get 'mypage', to: 'users#mypage'
+    get "search" => "searches#search"
+    resources :users, only: [:edit, :show, :index, :update, :destroy]
+    resources :posts, only: [:new, :index, :show, :edit, :create, :update, :destroy] do
+      resources :post_comments, only: [:create, :destroy]
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
