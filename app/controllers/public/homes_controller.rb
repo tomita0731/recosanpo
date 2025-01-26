6140a6372562449f0e4da028683a8ae7ee243115
EC2 Default User <ec2-user@ip-172-31-38-133.eu-north-1.compute.internal>
2025-01-26 05:48:53 +0000

    [Fix]card/publish_status

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     if user_signed_in?
-      @posts = Post.where(user_id: [current_user.id, *current_user.following_ids]).latest
+      @posts = Post.where(publish_status:1).where(user_id: [current_user.id, *current_user.following_ids]).latest
     end
   end
 
